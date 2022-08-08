b932ad158033dcf51009f8c7aa122d7f75eb8ad0
Pavel Sukhanov <psuhanov77@gmail.com>
2022-08-08 21:45:05 +0300

    add js code

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const backdrop = document.querySelector('#backdrop');
 content.addEventListener('click', openCard);
 backdrop.addEventListener('click', closeModal);
 
-// modal.classList.add('open')
-
-// setTimeout(function() {
-//     modal.classList.remove('open')
-// }, 2000)
+const technologies = [
+    {title: 'HTML', description: 'HTML Text', type: 'html', done: true},
+    {title: 'CSS', description: 'CSS Text', type: 'css', done: true},
+    {title: 'JavaScript', description: 'JavaScript Text', type: 'js', done: false},
+    {title: 'Git', description: 'Git Text', type: 'git', done: false},
+    {title: 'React', description: 'React Text', type: 'react', done: false},
+]
 
 function openCard() {
     modal.classList.add('open');
@@ -18,3 +20,28 @@ function openCard() {
 function closeModal() {
     modal.classList.remove('open');
 }
+
+function init() {
+    if (technologies.length === 0) {
+        content.innerHTML = '<p class="empty">Технологий пока нет. Добавьте первую</p>'
+    } else {
+        // let html = '';
+        // for (let i = 0; i < technologies.length; i++) {
+        //     const tech = technologies[i];
+        //     html += toCard(tech);
+        // }
+        // content.innerHTML = html
+        content.innerHTML = technologies.map(toCard).join('');
+    }
+}
+
+function toCard(tech) {
+    const doneClass = tech.done ? 'done' : '';
+    return `
+        <div class="card ${doneClass}">
+            <h3>${tech.title}</h3>
+        </div>
+    `
+}
+
+init();
