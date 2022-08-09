a2824ca69adbda775d58df381a89a552b655cd2c
Pavel Sukhanov <psuhanov77@gmail.com>
2022-08-09 20:29:18 +0300

    add styles in js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const modal = document.querySelector('#modal');
 const content = document.querySelector('#content');
 const backdrop = document.querySelector('#backdrop');
+const progress = document.querySelector('#progress');
 
 content.addEventListener('click', openCard);
 backdrop.addEventListener('click', closeModal);
@@ -22,6 +23,11 @@ function closeModal() {
 }
 
 function init() {
+    renderCards();
+    renderProgress();
+}
+
+function renderCards() {
     if (technologies.length === 0) {
         content.innerHTML = '<p class="empty">Технологий пока нет. Добавьте первую</p>'
     } else {
@@ -33,6 +39,37 @@ function init() {
         // content.innerHTML = html
         content.innerHTML = technologies.map(toCard).join('');
     }
+}
+
+function renderProgress() {
+    const percent = computeProgressPercent();
+
+    let background;
+
+    if(percent <= 30) {
+        background = '#e75a5a';
+    } else if (percent > 30 && percent <70) {
+        background = '#f99415';
+    } else {
+        background = '#73ba3c';
+    }
+
+    progress.style.background = background;
+    progress.style.width = percent + '%';
+    progress.textContent = percent ? percent + '%' : '';
+}
+
+function computeProgressPercent() {
+    if (technologies.length === 0) {
+        return 0;
+    }
+
+    let doneCount = 0;
+    for (let i = 0; i < technologies.length; i++) {
+        if (technologies[i].done) doneCount++;
+    }
+
+    return Math.round((100 * doneCount) / technologies.length);
 }
 
 function toCard(tech) {
